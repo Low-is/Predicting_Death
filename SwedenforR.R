028d0a3b79e2dd889a124db5460bd8f8bac8b645
Lois Randolph <lois.randolph@outlook.com>
2025-08-28 09:37:38 -0500

    Initial commit: add SwedenforR.R

---
+++ SwedenforR.R
@@ -0,0 +1,7 @@
+library(readxl)
+
+
+####################### Loading and processing data #######################
+SwedenforR <- read_excel("SwedenforR.xlsx")
+
+####################### Loading and processing data #######################
